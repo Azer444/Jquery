f0961011151eb17843892d0694263e3beb873b63
Azer444 <azerrh@code.edu.az>
2022-09-07 01:53:49 +0400

    Jquery

---
+++ assets/js/basket.js
@@ -0,0 +1,50 @@
+let products = [];
+
+if (localStorage.getItem("products") != null) {
+  products = JSON.parse(localStorage.getItem("products"))
+}
+
+let tableBody = document.querySelector(".table .table-body");
+
+
+for (const product of products) {
+  tableBody.innerHTML += `<tr>
+    <td data-id="${product.id}"><img src="${product.image}" height="100px" width="100px" class="card-img-top" alt="..."></td>
+    <td>${product.name}</td>
+    <td>${product.desc}</td>
+    <td>${product.count}</td>
+    <td><i style="color:red;font-size:25px;cursor:pointer;" class="fa-solid fa-trash-can delete-btn"></i></td>
+  </tr>`
+}
+
+
+document.querySelector("sup").innerText = getProductsCount(products);
+
+function getProductsCount(items) {
+  let resultCount = 0;
+  for (const item of items) {
+    resultCount += item.count
+  }
+  return resultCount;
+}
+
+let iconBtns = document.querySelectorAll(".delete-btn");
+
+iconBtns.forEach(btn => {
+  btn.addEventListener("click", function () {
+    let id = parseInt(this.parentNode.parentNode.firstElementChild.getAttribute("data-id"))
+
+    products = products.filter(m => m.id != id)
+
+    localStorage.setItem("products", JSON.stringify(products));
+
+    this.parentNode.parentNode.remove();
+
+    document.querySelector("sup").innerText = getProductsCount(products);
+  })
+});
+
+
+
+
+
